336d9df43a708d36e4c7c99ba75570a32aa713eb
verhneudinec <webarts@yandex.ru>
2020-12-14 23:26:08 +0800

    Working with Text

--- lib/main.dart
+++ lib/main.dart
@@ -23,51 +23,80 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
+      home: Scaffold(
+        appBar: AppBar(
           title: Text(widget.title),
           actions: [
             IconButton(icon: Icon(Icons.alarm), onPressed: _incrementCounter)
           ],
           bottom: PreferredSize(
               child: Text("Bottom of AppBar"),
-              preferredSize: Size.fromHeight(50))),
-      body: Center(
-        child: Text("Кнопка нажата $_counter раз",
-            style: Theme.of(context).textTheme.headline5),
+              preferredSize: Size.fromHeight(50)),
+        ),
+        body: Center(
+          child: Container(
+            color: Colors.black12,
+            width: double.infinity,
+            height: 50,
+            // работа с Text
+            //     child: Text(
+            //       "very long text very long text very long text very long text very long text ",
+            //       maxLines: 1,
+            //       overflow: TextOverflow.ellipsis,
+            //       style: TextStyle(
+            //         fontSize: 20,
+            //         color: Colors.blue,
+            //         decoration: TextDecoration.underline,
+            //         decorationColor: Colors.red,
+            //         decorationThickness: 5, // толщина decoration
+            //         letterSpacing: 5, // межбуквенные интервалы
+            //         wordSpacing: 5,
+            //       ),
+            //     ),
+            // ----------------------------------
+            // работа с RichText
+            child: RichText(
+              textAlign: TextAlign.center,
+              maxLines: 1,
+              text: TextSpan(
+                  text: "Green, ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.green,
+                    decoration: TextDecoration.underline,
+                    decorationColor: Colors.red,
+                    decorationThickness: 5, // толщина decoration
+                    letterSpacing: 5, // межбуквенные интервалы
+                    wordSpacing: 5,
+                  ),
+                  children: <InlineSpan>[
+                    TextSpan(
+                      text: "blue ",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 30,
+                      ),
+                    ),
+                    TextSpan(
+                        text: "and red",
+                        style: TextStyle(
+                          color: Colors.red,
+                          decorationColor: Colors.black,
+                          fontSize: 40,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "!",
+                            style: TextStyle(
+                              color: Colors.green,
+                            ),
+                          ),
+                        ]),
+                  ]),
+            ),
+          ),
+        ),
       ),
-      drawer: Container(
-          color: Colors.green,
-          width: 250,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              DrawerHeader(
-                child: Text("Drawer header"),
-                decoration: BoxDecoration(color: Colors.blue),
-              ),
-              ListTile(
-                title: Text("item #1"),
-                onTap: _incrementCounter,
-              ),
-              ListTile(
-                title: Text("item #2"),
-              )
-            ],
-          )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        child: Icon(Icons.add),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.mail), title: Text("Messages")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person), title: Text("Profile"))
-        ],
-      ),
-    ));
+    );
   }
 }
